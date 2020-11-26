5578be65f0d7df867c39f333d9f0bded734de24d
mingman123 <3532345033@qq.com>
2020-11-26 18:35:45 +0800

    未来战士 锤石

---
+++ a20191126pulsefire/a20191126pulsefire/js/index.js
@@ -0,0 +1,35 @@
+
+function TGDialogS(e){
+    // 利用milo库引入dialog组件
+    need("biz.dialog",function(Dialog){
+        Dialog.show({
+            id:e,
+            bgcolor:'#000', //弹出“遮罩”的颜色，格式为"#FF6600"，可修改，默认为"#fff"
+            opacity:50 //弹出“遮罩”的透明度，格式为｛10-100｝，可选
+        });
+    });
+}
+function closeDialog(){
+    // 利用milo库引入dialog组件
+    need("biz.dialog",function(Dialog){
+        Dialog.hide();
+    });
+}
+
+var player
+function showVideoDia(vid) {
+	player = new Txplayer({
+		containerId: 'container',
+		vid: vid,
+		width: '100%',
+		height: '100%',
+		autoplay: true
+		});
+	TGDialogS('dia1');
+}
+$(".dia-close").click(function(){
+	console.log(1111)
+	$(".video-box").html('');
+	player.mute();
+	console.log(333)
+})
