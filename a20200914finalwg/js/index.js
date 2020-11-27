a66418079f65a7ea8dbe8ddaa79c81b00f041826
mingman123 <3532345033@qq.com>
2020-11-27 11:12:02 +0800

    lol总决赛赛事wg

---
+++ a20200914finalwg/js/index.js
@@ -0,0 +1,17 @@
+function TGDialogS(e) {
+      // 利用milo库引入dialog组件
+      need("biz.dialog", function (Dialog) {
+        Dialog.show({
+          id: e,
+          bgcolor: '#000', //弹出“遮罩”的颜色，格式为"#FF6600"，可修改，默认为"#fff"
+          opacity: 80 //弹出“遮罩”的透明度，格式为｛10-100｝，可选
+        });
+      });
+    }
+
+    function closeDialog() {
+      // 利用milo库引入dialog组件
+      need("biz.dialog", function (Dialog) {
+        Dialog.hide();
+      });
+    }
